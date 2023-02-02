0a458314fdddf0cc9977d7e7bb498df6ea74f06d
ASUS <ASUS@LAPTOP-EA2VLOMF>
2023-02-02 13:40:37 +0530

    second commit

--- Dayone/src/org/tnsif/intro/UserInputDemo.java
+++ Dayone/src/org/tnsif/intro/UserInputDemo.java
@@ -5,21 +5,23 @@ public class UserInputDemo {
 	public static void main(String[]args) {
 	Scanner sc=new Scanner(System.in);
 	int x=sc.nextInt();
-	float y=sc.nextFloat();
-	char c=sc.next().charAt(0);
-	String name=sc.next();
-	String str=sc.nextLine();
-	double d=sc.nextDouble();
-	System.out.println("The value of x is:"+x);
-	System.out.println("The value of y is:"+y);
-	System.out.println("The value of c is:"+c);
-	System.out.println("The value of name is:"+name);
-	System.out.println("The value of str is:"+str);
-	 
-	System.out.println("The value of d is:"+d);
+	
+	//float y=sc.nextFloat();
+	//char c=sc.next().charAt(0);
+	//String name=sc.next();
+//	String str=sc.nextLine();
+//	double d=sc.nextDouble();
+System.out.println("The value of x is:"+x);
+//	System.out.println("The value of y is:"+y);
+//	System.out.println("The value of c is:"+c);
+//	System.out.println("The value of name is:"+name);
+//	System.out.println("The value of str is:"+str);
+//	 
+//	System.out.println("The value of d is:"+d);
 	sc.close();
 	}
 	
 }
+
 
 
