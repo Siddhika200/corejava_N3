0e83dc26af4ba3a3449290464bc16f2e3d2ab9d4
ASUS <ASUS@LAPTOP-EA2VLOMF>
2023-02-02 13:05:30 +0530

    Second commit

---
+++ Dayone/src/org/tnsif/intro/DatatypesDemo.java
@@ -0,0 +1,51 @@
+package org.tnsif.intro;
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		//byte
+		byte a =127;
+		//int
+		int b =128;
+		long b1=22222222l;
+		//float
+		float c=12/5;
+		float d=5/2.5f;
+		//double
+		double e = 12d/4;
+		double f =12d/5d;
+			
+		//character
+		char g='a';
+		
+		//ASCII value
+		int z='b';
+		
+		//string 
+		String name="siddhika";
+		
+		//boolean
+		boolean y =true;
+		
+		
+		
+ System.out.println("The name is " +name);		
+ System.out.println("The value of a is "+a);
+ System.out.println("The value of b,b1 is "+b+" "+b1);
+ System.out.println("The value of c is "+c);
+ System.out.println("The value of d is "+d);
+ System.out.println("The value of c is "+c);
+ System.out.println("The value of d is "+d);
+ System.out.println("The value of e is "+e);
+ System.out.println("The value of f is "+f);
+ System.out.println("The value of g is "+g);
+ System.out.println("The value of z is "+z);
+ System.out.println("The value of y is "+y);
+ 
+ 
+ 
+ 
+ 
+	}
+
+}
