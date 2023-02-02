43de04674c4100e29b567fd616de61d387e8abf1
ASUS <ASUS@LAPTOP-EA2VLOMF>
2023-02-02 11:02:57 +0530

    First Commit

---
+++ Dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,12 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args)
+	{	
+		System.out.println("Welcome yo tnsif");
+		// TODO Auto-generated method stub
+
+	}
+
+}
