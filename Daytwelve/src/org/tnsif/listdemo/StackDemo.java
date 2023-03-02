6b43b66a1f2aa651ca0131d040b7b1d6d8c74805
ASUS <ASUS@LAPTOP-EA2VLOMF>
2023-03-02 20:54:15 +0530

    Second commit

---
+++ Daytwelve/src/org/tnsif/listdemo/StackDemo.java
@@ -0,0 +1,22 @@
+package org.tnsif.listdemo;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack<String> obj=new Stack<>();
+		obj.push("Dhanshri");
+		obj.push("Neha");
+		obj.push("Gaurav");
+		obj.push("Priya");
+		System.out.println(obj);
+		obj.pop();
+		System.out.println(obj);
+
+		
+		
+
+	}
+
+}
